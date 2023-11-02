3e7b78e24682e7e524188d874a099853b3f57fb6
user <gkamilv@gmail.com>
2023-11-02 22:49:51 +0500

    chore: Добавлено правило генерации диапазона зарплат JobFactory

--- src/fixtures/jobs.py
+++ src/fixtures/jobs.py
@@ -2,6 +2,7 @@ import factory
 from models import Job
 from datetime import datetime
 from factory_boy_extra.async_sqlalchemy_factory import AsyncSQLAlchemyModelFactory
+import random
 
 
 class JobFactory(AsyncSQLAlchemyModelFactory):
@@ -12,8 +13,8 @@ class JobFactory(AsyncSQLAlchemyModelFactory):
     user_id = factory.Faker('pyint')
     title = factory.Faker('job')
     description = factory.Faker('pystr')
-    salary_from = factory.Faker('pyfloat')
-    salary_to = factory.Faker('pyfloat')
+    salary_from = factory.Faker('pyfloat', min_value=0, max_value=100000)
+    salary_to = factory.LazyAttribute((lambda x: x.salary_from + random.randrange(1, 100000)))
     is_active = factory.Faker("pybool")
     created_at = factory.LazyFunction(datetime.utcnow)
 
