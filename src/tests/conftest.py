59f16997c69340206e5314ddb6a22c3e080de8c7
user <gkamilv@gmail.com>
2023-11-05 19:58:12 +0500

    feat: Добавлены тесты эндпроинтов /user

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,7 +1,11 @@
 import asyncio
+
+from httpx import AsyncClient
 from sqlalchemy import inspect
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+
+from core.security import create_access_token
 from fixtures import UserFactory, ResponseFactory, JobFactory
 from fastapi.testclient import TestClient
 from main import app
@@ -9,6 +13,9 @@ import pytest
 import pytest_asyncio
 from unittest.mock import MagicMock
 from db_settings import SQLALCHEMY_DATABASE_URL
+from models import User
+from schemas import TokenSchema
+from dependencies import get_db
 
 
 @pytest.fixture()
@@ -49,7 +56,7 @@ async def sa_session():
 
 
 @pytest_asyncio.fixture()
-async def test_user(sa_session: AsyncSession):
+async def test_user(sa_session: AsyncSession) -> User:
     new_user = UserFactory.build()
     sa_session.add(new_user)
 
@@ -65,3 +72,28 @@ def setup_factories(sa_session: AsyncSession) -> None:
     UserFactory.session = sa_session
     JobFactory.session = sa_session
     ResponseFactory.session = sa_session
+
+
+@pytest_asyncio.fixture()
+async def access_token(test_user: User):
+    token = TokenSchema(
+        access_token=create_access_token({"sub": test_user.email}),
+        token_type="Bearer"
+    )
+    return token
+
+
+@pytest_asyncio.fixture()
+async def test_app(sa_session, access_token: TokenSchema):
+    app.dependency_overrides[get_db] = lambda: sa_session
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        client.headers["Authorization"] = f"Bearer {access_token.access_token}"
+        yield client
+
+@pytest_asyncio.fixture()
+async def test_app_unauthorized(sa_session, access_token: TokenSchema):
+    app.dependency_overrides[get_db] = lambda: sa_session
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
---
+++ src/tests/routers/test_users.py
@@ -0,0 +1,138 @@
+import pytest
+from fastapi import status
+from pydantic import ValidationError
+from fixtures import UserFactory
+from models import User
+from schemas import UserInSchema, UserUpdateSchema
+
+
+@pytest.mark.asyncio
+async def test_unauthorized_access(test_app_unauthorized):
+    deleted_user = await test_app_unauthorized.delete(url="/users")
+
+    assert deleted_user.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+async def test_create_user(test_app_unauthorized):
+    user = UserInSchema(
+        name='User',
+        email='email@mail.com',
+        password='password123',
+        password2='password123',
+        is_company=False)
+
+    registered_user = await test_app_unauthorized.post(url="/users", json=user.model_dump())
+
+    assert registered_user.status_code == status.HTTP_200_OK
+    assert user.name == registered_user.json()["name"]
+    assert user.email == registered_user.json()["email"]
+
+
+@pytest.mark.asyncio
+async def test_create_user_email_validation_error(test_app_unauthorized):
+    with pytest.raises(ValidationError):
+        user = UserInSchema(
+            name='User',
+            email='not_correct_email',
+            password='password123',
+            password2='password123',
+            is_company=False)
+
+        registered_user = await test_app_unauthorized.put(url="/users", json=user.model_dump())
+
+        assert registered_user.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
+async def test_create_user_password_validation_error(test_app_unauthorized):
+    with pytest.raises(ValidationError):
+        user = UserInSchema(
+            name='User',
+            email='email@mail.com',
+            password='password_1',
+            password2='password_2',
+            is_company=False)
+
+        registered_user = await test_app_unauthorized.post(url="/users", json=user.model_dump())
+
+        assert registered_user.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
+async def test_create_registered_user(test_app_unauthorized, sa_session):
+    old_user = UserFactory.build()
+    sa_session.add(old_user)
+    sa_session.flush()
+
+    new_user = UserInSchema(
+        name='User',
+        email=old_user.email,
+        password='password123',
+        password2='password123',
+        is_company=False
+    )
+
+    registered_user = await test_app_unauthorized.post(url="/users", json=new_user.model_dump())
+
+    assert registered_user.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.asyncio
+async def test_read_users(test_app):
+    jobs = await test_app.get("/users")
+    assert jobs.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
+async def test_update_user(test_user, test_app):
+    updated_test_user = UserUpdateSchema(
+        name='new_name',
+        email='new_email@mail.com',
+        is_company=True
+    )
+
+    updated_user = await test_app.put(url="/users", json=updated_test_user.model_dump())
+
+    assert updated_user.status_code == status.HTTP_200_OK
+    assert updated_test_user.name == updated_user.json()["name"]
+    assert updated_test_user.email == updated_user.json()["email"]
+
+
+@pytest.mark.asyncio
+async def test_update_user_to_same_email(test_user: User, test_app):
+    updated_test_user = UserUpdateSchema(
+        name='new_name',
+        email=test_user.email,
+        is_company=True
+    )
+
+    updated_user = await test_app.put(url="/users", json=updated_test_user.model_dump())
+
+    assert updated_user.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.asyncio
+async def test_update_user_to_blocked_email(sa_session, test_user: User, test_app):
+    old_user = UserFactory.build()
+    sa_session.add(old_user)
+    sa_session.flush()
+
+    updated_test_user = UserUpdateSchema(
+        name='new_name',
+        email=old_user.email,
+        is_company=True
+    )
+
+    updated_user = await test_app.put(url="/users", json=updated_test_user.model_dump())
+
+    assert updated_user.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.asyncio
+async def test_delete_user(test_app, test_user: User):
+    deleted_user = await test_app.delete(url="/users")
+
+    assert deleted_user.status_code == status.HTTP_200_OK
+    assert deleted_user.json()["email"] == test_user.email
+
